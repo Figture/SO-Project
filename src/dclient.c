82d7fc42ba5145460da97bdbab9d31ed9f0af8ce
Figture <figture24548@gmail.com>
2025-04-16 15:39:40 +0100

    Try

--- src/dclient.c
+++ src/dclient.c
@@ -6,40 +6,50 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+int main(int argc, char *argv[])
+{
 
-
-int main(int argc, char *argv[]){
-
-	if(argc<3){
+	if (argc < 2)
+	{
 		printf("Usage:\n");
-        printf("Index document: ./dclient -a [title] [authors] [year] [path]\n");
-        printf("Check key: ./dclient -c [key]\n");
-        printf("Delete key: ./dclient -d [key]\n");
-        printf("Search keyword of given key: ./dclient -l [key] [keyword]\n");
-        printf("Search keyword of all keys: ./dclient -s [keyword]\n");
-		// printf("Stop server: ./dclient -f\n");
-        return 1;
+		printf("Index document: ./dclient -a [title] [authors] [year] [path]\n");
+		printf("Check key: ./dclient -c [key]\n");
+		printf("Delete key: ./dclient -d [key]\n");
+		printf("Search keyword of given key: ./dclient -l [key] [keyword]\n");
+		printf("Search keyword of all keys: ./dclient -s [keyword]\n");
+		printf("Stop server: ./dclient -f\n");
+		return 1;
 	}
 
 	// Make the MSG
 	MSG input;
-	if(argc > 1) { //perguntar sobre isto ||joão Oliveira
-		strcpy(input.flag,argv[1]);
-		for (int i = 2; i < argc; i++) {
-			strcpy(input.argv[i-2], argv[i]);
+	if (argc > 1)
+	{ // perguntar sobre isto ||joão Oliveira
+		strcpy(input.flag, argv[1]);
+		if (argc == 2 && (strcmp(input.flag, "-f") != 0)) 
+		{
+			printf("Usage:\n");
+			printf("Index document: ./dclient -a [title] [authors] [year] [path]\n");
+			printf("Check key: ./dclient -c [key]\n");
+			printf("Delete key: ./dclient -d [key]\n");
+			printf("Search keyword of given key: ./dclient -l [key] [keyword]\n");
+			printf("Search keyword of all keys: ./dclient -s [keyword]\n");
+			printf("Stop server: ./dclient -f\n");
+			return 1;
+		}
+		for (int i = 2; i < argc; i++)
+		{
+			strcpy(input.argv[i - 2], argv[i]);
 		}
 	}
 	input.pid = getpid();
-	
+
 	// ligar o fifo
 	int fdin = open(C_TO_S, O_WRONLY);
-	
+
 	write(fdin, &input, sizeof(MSG));
 
-
 	// fifo output por cliente
-	
-
 
 	/*
 	if(strcmp(argv[1],"-a")==0){
